2cfd5806229221d9d6c15803bac1204ced3d2265
Michael Lesniak <mail@mlesniak.com>
2020-01-21 23:36:59 +0100

    Add random boxes

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"image/color"
 	"log"
+	"math/rand"
 	"os"
+
+	_ "image/png"
 )
-import "github.com/hajimehoshi/ebiten"
+
+const width = 800
+const height = 600
+const numBoxes = 10
+
+type Box struct {
+	x, y float64
+	w, h float64
+}
+
+var boxImage *ebiten.Image
+var boxes = make([]Box, numBoxes)
 
 func update(screen *ebiten.Image) error {
 	checkExit()
+
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
 
 	background(screen)
+	for _, box := range boxes {
+		op := &ebiten.DrawImageOptions{}
+		w, h := boxImage.Size()
+		scale := box.w / float64(w)
+		op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
+		op.GeoM.Scale(scale, scale)
+		op.GeoM.Translate(box.x, box.y)
+		screen.DrawImage(boxImage, op)
+	}
+
 	return nil
 }
 
@@ -28,7 +55,19 @@ func checkExit() {
 }
 
 func main() {
-	if err := ebiten.Run(update, 800, 600, 1, "Physics Demo"); err != nil {
+	boxImage, _, _ = ebitenutil.NewImageFromFile("box.png", ebiten.FilterDefault)
+	boxWidth := 40.0
+	boxHeight := 40.0
+	for i := 0; i < numBoxes; i++ {
+		boxes = append(boxes, Box{
+			x: rand.Float64() * (width - boxWidth),
+			y: rand.Float64()*50 + boxHeight/2.0,
+			w: boxWidth,
+			h: boxHeight,
+		})
+	}
+
+	if err := ebiten.Run(update, width, height, 1, "Physics Demo"); err != nil {
 		log.Fatal(err)
 	}
 }
