98260fdde763e97382c2fd8073165b8289a2fa60
Michael Lesniak <mail@mlesniak.com>
2020-01-22 00:34:33 +0100

    Correct simulation

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/hajimehoshi/ebiten v1.10.3
 	github.com/jakecoffman/cp v0.1.0
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8
 )
--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"github.com/jakecoffman/cp"
+	"golang.org/x/image/colornames"
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"os"
+	"time"
 
 	_ "image/png"
 )
@@ -16,11 +19,12 @@ var space *cp.Space // Simulation space
 
 const width = 800
 const height = 600
-const numBoxes = 1
+const numBoxes = 10
 
 type Box struct {
 	x, y float64
 	w, h float64
+	r    float64
 }
 
 var boxImage *ebiten.Image
@@ -43,14 +47,16 @@ func update(screen *ebiten.Image) error {
 func drawBoxes(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	space.EachBody(func(body *cp.Body) {
-		// TODO How to add information about the created object here?
-		op.GeoM.Reset()
-		op.GeoM.Translate(body.Position().X, body.Position().Y)
-		w, _ := boxImage.Size()
-		scale := 40.0 / float64(w)
-		//op.GeoM.Translate(-float64(w)/2, -float64(h)/2)
-		op.GeoM.Scale(scale, scale)
-		screen.DrawImage(boxImage, op)
+		switch body.UserData {
+		case "box":
+			op.GeoM.Reset()
+			//log.Printf("%v: %f/%f\n", body, body.Position().X, body.Position().Y)
+			op.GeoM.Translate(body.Position().X, body.Position().Y)
+			screen.DrawImage(boxImage, op)
+		case "line":
+			//log.Printf("%v: %f/%f\n", body, body.Position().X, body.Position().Y)
+			ebitenutil.DrawLine(screen, 0, body.Position().Y, width, body.Position().Y, colornames.Yellow)
+		}
 	})
 }
 
@@ -69,14 +75,24 @@ func main() {
 
 	space = cp.NewSpace()
 	space.Iterations = 1
-	space.SetGravity(cp.Vector{0, 100})
+	space.SetGravity(cp.Vector{0, 800})
+
+	// Add floor
+	bf := cp.NewStaticBody()
+	h := 100.0
+	bf.SetPosition(cp.Vector{width / 2, height - h})
+	bf.UserData = "line"
+	sf := cp.NewBox(bf, width, h/2, 1.0)
+	space.AddBody(bf)
+	space.AddShape(sf)
 
 	for _, box := range boxes {
 		body := cp.NewBody(1.0, cp.INFINITY)
 		body.SetPosition(cp.Vector{X: box.x, Y: box.y})
+		body.UserData = "box"
 
 		shape := cp.NewBox(body, box.w, box.h, 0.0)
-		shape.SetElasticity(0)
+		shape.SetElasticity(1.0)
 		shape.SetFriction(0)
 
 		space.AddBody(body)
@@ -90,9 +106,11 @@ func main() {
 }
 
 func initBoxes() {
+	rand.Seed(time.Now().Unix())
 	boxImage, _, _ = ebitenutil.NewImageFromFile("box.png", ebiten.FilterDefault)
-	boxWidth := 40.0
-	boxHeight := 40.0
+	w, h := boxImage.Size()
+	boxWidth := float64(w)
+	boxHeight := float64(h)
 	for i := 0; i < numBoxes; i++ {
 		log.Println("Creating box", i)
 		boxes = append(boxes, Box{
@@ -100,6 +118,7 @@ func initBoxes() {
 			y: rand.Float64()*50 + boxHeight/2.0,
 			w: boxWidth,
 			h: boxHeight,
+			r: rand.Float64() * 2 * math.Pi,
 		})
 	}
 }
