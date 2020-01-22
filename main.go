bfcafe9cb99bc328fa5c7e4e112574f9296b9d8f
Michael Lesniak <mail@mlesniak.com>
2020-01-22 08:31:59 +0100

    Cleanup

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 	"github.com/jakecoffman/cp"
 	"golang.org/x/image/colornames"
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 	"time"
 
 	_ "image/png"
 )
-
-var space *cp.Space // Simulation space
 
 const width = 800
 const height = 600
@@ -28,28 +28,51 @@ type Box struct {
 	r    float64
 }
 
+var space *cp.Space // Simulation space
 var boxImage *ebiten.Image
 var boxes = []Box{}
+var lastClicked = 0 // Frames to wait until the next mouse click is registered.
 
-var down = 0
+func main() {
+	boxImage, _, _ = ebitenutil.NewImageFromFile("box.png", ebiten.FilterDefault)
+	initBoxes()
+
+	space = cp.NewSpace()
+	space.Iterations = 1
+	space.SetGravity(cp.Vector{0, 800})
+
+	addFloor()
+
+	for _, box := range boxes {
+		body := addBoxToPhysics(box)
+		log.Printf("Creating body %v\n", body)
+	}
+
+	if err := ebiten.Run(update, width, height, 1, "Physics Demo"); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func update(screen *ebiten.Image) error {
-	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && down < 0 {
-		down = 10 // FPS fix
+	// Input handling.
+	checkExit()
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && lastClicked < 0 {
+		lastClicked = 10 // FPS fix
 		x, y := ebiten.CursorPosition()
 		b := addBox(float64(x), float64(y))
 		addBoxToPhysics(b)
 	} else {
-		down--
+		lastClicked--
 	}
 
-	checkExit()
+	// Next step in engine.
 	space.Step(1.0 / float64(ebiten.MaxTPS()))
 
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
 
+	// Show results.
 	background(screen)
 	drawBoxes(screen)
 
@@ -57,10 +80,17 @@ func update(screen *ebiten.Image) error {
 }
 
 func drawBoxes(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.ColorM.Scale(200.0/255.0, 200.0/255.0, 200.0/255.0, 1)
+
 	space.EachBody(func(body *cp.Body) {
 		switch body.UserData {
 		case "box":
-			ebitenutil.DrawRect(screen, body.Position().X-boxHeight/2, body.Position().Y-boxWidth/2, boxWidth, boxHeight, color.RGBA{80, 80, 80, 255})
+			fmt.Printf("%.2f\n", body.Rotation().ToAngle())
+			op.GeoM.Reset()
+			op.GeoM.Rotate(body.Angle())
+			op.GeoM.Translate(body.Position().X-boxWidth/2, body.Position().Y-boxHeight/2)
+			screen.DrawImage(boxImage, op)
 		case "line":
 			ebitenutil.DrawLine(screen, 0, body.Position().Y, width, body.Position().Y, colornames.Yellow)
 		}
@@ -77,39 +107,22 @@ func checkExit() {
 	}
 }
 
-func main() {
-	initBoxes()
-
-	space = cp.NewSpace()
-	space.Iterations = 1
-	space.SetGravity(cp.Vector{0, 800})
-
+func addFloor() {
 	bf := cp.NewStaticBody()
 	bf.SetPosition(cp.Vector{width / 2, height})
 	bf.UserData = "line"
 	sf := cp.NewBox(bf, width, 0, 0.0)
 	space.AddBody(bf)
 	space.AddShape(sf)
-
-	for _, box := range boxes {
-		body := addBoxToPhysics(box)
-		log.Printf("Creating body %v\n", body)
-	}
-
-	if err := ebiten.Run(update, width, height, 1, "Physics Demo"); err != nil {
-		log.Fatal(err)
-	}
 }
 
 func addBoxToPhysics(box Box) *cp.Body {
-	body := cp.NewBody(10.0, cp.INFINITY)
+	body := cp.NewBody(100.0, cp.INFINITY)
 	body.SetPosition(cp.Vector{X: box.x, Y: box.y})
+	body.SetAngle(box.r)
 	body.UserData = "box"
 
-	shape := cp.NewBox(body, box.w, box.h, box.r)
-	shape.SetElasticity(1.0)
-	shape.SetFriction(0)
-
+	shape := cp.NewBox(body, box.w, box.h, 0.0)
 	space.AddBody(body)
 	space.AddShape(shape)
 	return body
@@ -128,6 +141,7 @@ func addBox(px, py float64) Box {
 		y: py,
 		w: boxWidth,
 		h: boxHeight,
+		r: rand.Float64() * 2 * math.Pi,
 	}
 	boxes = append(boxes, box)
 	return box
